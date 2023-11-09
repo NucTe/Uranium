417b0260d3c35409e9583988873479c0abfc83f9
RealCatDev <real.catdev@gmail.com>
2023-11-09 21:46:53 +0100

    Initial commit!

---
+++ src/main/java/me/nuclearteam/uranium/Uranium.java
@@ -0,0 +1,16 @@
+package me.nuclearteam.uranium;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Uranium extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
